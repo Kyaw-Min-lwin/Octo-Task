d026b4684a5c6d1b077b2db1d5bd1c759b0af4f9
kl25abc <kl25abc@herts.ac.uk>
2025-12-30 01:02:09 +0000

    db connection update

--- app.py
+++ app.py
@@ -27,8 +27,6 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = SQLALCHEMY_TRACK_MODIFICATIONS
 db.init_app(app)
 migrate = Migrate(app, db)
 
-with app.app_context():
-    db.create_all()
 # --- AUTH ROUTES ---
 
 
--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI")
+SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
